29b0c6187153025f4068819389d18a935304c7b5
corneliouz Bett <kbett68@gmail.com>
2020-08-25 12:49:54 +0300

    Implement condition search

--- api/src/main/java/org/openmrs/module/fhir2conditions/api/impl/FhirConditionServiceImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2conditions/api/impl/FhirConditionServiceImpl.java
@@ -9,6 +9,7 @@
  */
 package org.openmrs.module.fhir2conditions.api.impl;
 
+import ca.uhn.fhir.rest.annotation.Sort;
 import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.param.DateRangeParam;
@@ -18,11 +19,13 @@ import ca.uhn.fhir.rest.param.TokenAndListParam;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-import org.hl7.fhir.r4.model.Condition;
 import org.openmrs.annotation.OpenmrsProfile;
+import org.openmrs.module.fhir2.FhirConstants;
 import org.openmrs.module.fhir2.api.FhirConditionService;
 import org.openmrs.module.fhir2.api.dao.FhirConditionDao;
 import org.openmrs.module.fhir2.api.impl.BaseFhirService;
+import org.openmrs.module.fhir2.api.search.SearchQuery;
+import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
 import org.openmrs.module.fhir2.api.translators.ConditionTranslator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
@@ -35,7 +38,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Getter(AccessLevel.PROTECTED)
 @Component("fhir2conditions.fhirConditionServiceImpl")
 @OpenmrsProfile(openmrsPlatformVersion = "2.0.* - 2.1.*")
-public class FhirConditionServiceImpl extends BaseFhirService<Condition, org.openmrs.module.emrapi.conditionslist.Condition> implements FhirConditionService {
+public class FhirConditionServiceImpl extends BaseFhirService<org.hl7.fhir.r4.model.Condition, org.openmrs.module.emrapi.conditionslist.Condition> implements FhirConditionService {
 	
 	@Autowired
 	private FhirConditionDao<org.openmrs.module.emrapi.conditionslist.Condition> dao;
@@ -43,15 +46,27 @@ public class FhirConditionServiceImpl extends BaseFhirService<Condition, org.ope
 	@Autowired
 	private ConditionTranslator<org.openmrs.module.emrapi.conditionslist.Condition> translator;
 	
-	@Override
-	public IBundleProvider searchConditions(ReferenceAndListParam referenceAndListParam,
-	        TokenAndListParam tokenAndListParam, TokenAndListParam tokenAndListParam1, DateRangeParam dateRangeParam,
-	        QuantityAndListParam quantityAndListParam, DateRangeParam dateRangeParam1, SortSpec sortSpec) {
-		return null;
-	}
+	@Autowired
+	private SearchQuery<org.openmrs.module.emrapi.conditionslist.Condition, org.hl7.fhir.r4.model.Condition, FhirConditionDao<org.openmrs.module.emrapi.conditionslist.Condition>, ConditionTranslator<org.openmrs.module.emrapi.conditionslist.Condition>> searchQuery;
 	
 	@Override
-	public Condition saveCondition(Condition condition) {
-		return null;
+	public IBundleProvider searchConditions(ReferenceAndListParam patientParam, TokenAndListParam code,
+	        TokenAndListParam clinicalStatus, DateRangeParam onsetDate, QuantityAndListParam onsetAge,
+	        DateRangeParam recordedDate, TokenAndListParam tokenAndListParam2, DateRangeParam dateRangeParam2, @Sort SortSpec sort) {
+		
+		SearchParameterMap theParams = new SearchParameterMap()
+		        .addParameter(FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER, patientParam)
+		        .addParameter(FhirConstants.CODED_SEARCH_HANDLER, code)
+		        .addParameter(FhirConstants.CONDITION_CLINICAL_STATUS_HANDLER, clinicalStatus)
+		        .addParameter(FhirConstants.QUANTITY_SEARCH_HANDLER, onsetAge)
+		        .addParameter(FhirConstants.DATE_RANGE_SEARCH_HANDLER, "onsetDate", onsetDate)
+		        .addParameter(FhirConstants.DATE_RANGE_SEARCH_HANDLER, "dateCreated", recordedDate).setSortSpec(sort);
+		
+		return searchQuery.getQueryResults(theParams, dao, translator);
+	}
+
+	@Override
+	public org.hl7.fhir.r4.model.Condition saveCondition(org.hl7.fhir.r4.model.Condition condition) {
+		return translator.toFhirResource(dao.createOrUpdate(translator.toOpenmrsType(condition)));
 	}
 }
